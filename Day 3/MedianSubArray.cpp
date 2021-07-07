b5a45f9b31e14d7fc78cb484dbdcbd5330f22040
mayank0803 <60825283+mayank0803@users.noreply.github.com>
2021-07-07 22:15:11 +0530

    Add files via upload

---
+++ Day 3/MedianSubArray.cpp
@@ -0,0 +1,19 @@
+#include"bits/stdc++.h"
+using namespace std;
+int main(){
+	int n,m,i;
+	cin >> n>> m;
+	int a1[n];
+	for(i=0;i<n;i++)
+	{
+		cin>>a1[i];
+	}
+	for(i=0;i<n;i++){
+		for(j=i;j<m;j++){
+			sort(a[i],a[i+m]);
+			int l=a[i+m].length();
+			if(l%2!=0){
+				cout<<a[]
+			}
+		}
+	}

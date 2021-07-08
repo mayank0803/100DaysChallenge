fe5af7c6b388d026cc7a3b6db09332e78ca385fb
mayank0803 <60825283+mayank0803@users.noreply.github.com>
2021-07-08 18:20:47 +0530

    Update MedianSubArray.cpp

--- Day 3/MedianSubArray.cpp
+++ Day 3/MedianSubArray.cpp
@@ -1,21 +1,44 @@
-// kal karunga
+/*
+You have been given an array/list ‘ARR’ of integers consisting of ‘N’ integers. You are also given a size ‘M’. 
+You need to display the median of all the subarrays of size ‘M’ and it is starting from the very left of the array.
+Median is the middle value in an ordered integer array/list. If the size of the array/list is even there is no middle element. 
+So the median is the mean of two middle values in an even size array/list.
+Your task is to return the median of all the subarrays whose size is ‘M’.
+Example:
+Let’s say you have an array/list [1,4,3,5] and ‘M’ is 3.Then the first subarray of size 3 is [1,4,3] whose median is 3.
+Then the second subarray of size 3 is [4,3,5] whose median is 4. Therefore the median of all the subarrays of size 3 is [3.0,4.0].
+*/
 
 #include"bits/stdc++.h"
 using namespace std;
-int main(){
-	int n,m,i;
-	cin >> n>> m;
-	int a1[n];
-	for(i=0;i<n;i++)
+int main ()
+{
+  int n, m, i;
+  cin >> n >> m;
+  int a1[n];
+  for (i = 0; i < n; i++)
+    {
+      cin >> a1[i];
+    }
+	//loop will run n-m time,no need to iterate every element
+  for (i = 0; i <= n - m; i++)
+    {
+	  // sort the subarray
+      sort (a1, a1 + m);
+      double med;
+	  //even or odd
+      if (m % 2 != 0)
 	{
-		cin>>a1[i];
+	  med = (double (a1[(m + i) / 2]));
+	  cout << med << " ";
 	}
-	for(i=0;i<n;i++){
-		for(j=i;j<m;j++){
-			sort(a[i],a[i+m]);
-			int l=a[i+m].length();
-			if(l%2!=0){
-				cout<<a[]
-			}
-		}
+      else
+	{
+	  double mid1 = (m + i) / 2.0;
+	  int y = round (mid1);
+	  int mid2 = y - 1;
+	  med = (double (a1[y] + a1[mid2])) /2.0;
+	  cout << med << " ";
 	}
+    }
+}

ffc7c431963b8840f11b1a92a952c826ce4f1191
mayank0803 <60825283+mayank0803@users.noreply.github.com>
2021-07-14 15:49:52 +0530

    Update and rename BreakPrison.cpp to Permutation.cpp

---
+++ Day 4/Permutation.cpp
@@ -0,0 +1,30 @@
+/*Given a string S. The task is to print all permutations of a given string.
+
+ 
+
+Example 1:
+
+Input: ABC
+Output:
+ABC ACB BAC BCA CAB CBA
+Explanation:
+Given string ABC has permutations in 6 
+forms as ABC, ACB, BAC, BCA, CAB and CBA .
+
+SOLN:*/
+void permute(string a, int l, int r)
+{
+    // Base case
+    if (l == r)
+        cout<<a<<endl;
+    else
+    {
+        // Permutations made
+        for (int i = l; i <= r; i++)
+        {
+            swap(a[l], a[i]);
+            permute(a, l+1, r);
+            swap(a[l], a[i]);
+        }
+    }
+}
